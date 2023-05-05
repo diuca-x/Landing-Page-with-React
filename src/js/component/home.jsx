59705907e35c4dec7035e0a0cd31b3305945b539
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-05-05 16:20:50 +0000

    removed borders

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,12 +10,12 @@ const Home = () => {
 		<>
 			<div className="text-center container">
 				<div className="row justify-content-center">
-					<div className=" col-12 col-md-6 col-xl-3 justify-content-center border border-primary">
+					<div className=" col-12 col-md-6 col-xl-3 justify-content-center ">
 						<Card title= "Card n°1" text= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus in nulla."
 						button_label= "Find out about n°1" button_link="goggle.com" 
 						img= "https://www.thebusinessdesk.com/_files/images/feb_19/The-former-Royal-Angus-hotel-DoubleTree-by-Hilton.-e1549306563820-500x325.jpg"/>				
 					</div>
-					<div className=" col-12 col-md-6 col-xl-3 justify-content-center border border-primary">
+					<div className=" col-12 col-md-6 col-xl-3 justify-content-center ">
 						<Card title= "Card n°2" text= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus in nulla."
 						button_label= "Find out about n°2" button_link="goggle.com" 
 						img= "https://www.thebusinessdesk.com/_files/images/feb_19/The-former-Royal-Angus-hotel-DoubleTree-by-Hilton.-e1549306563820-500x325.jpg"/>				
@@ -30,9 +30,7 @@ const Home = () => {
 						button_label= "Find out about n°4" button_link="goggle.com" 
 						img= "https://www.thebusinessdesk.com/_files/images/feb_19/The-former-Royal-Angus-hotel-DoubleTree-by-Hilton.-e1549306563820-500x325.jpg"/>				
 					</div>
-				</div>
-				
-			
+				</div>	
 			</div>
 			<Footer/>
 		</>
